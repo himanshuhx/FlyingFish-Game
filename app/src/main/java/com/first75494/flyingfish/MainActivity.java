86b6d49f64d32cc55e8eba96dc8a57c78df35a46
himanshuhx <himanshuhx1221.com>
2020-06-10 17:36:35 +0530

    Initial commit

---
+++ app/src/main/java/com/first75494/flyingfish/MainActivity.java
@@ -0,0 +1,14 @@
+package com.first75494.flyingfish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
