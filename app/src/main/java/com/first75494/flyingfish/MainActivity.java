89ef24e21a67192f278d50278ba6fd15d94173bf
himanshuhx <himanshuhx1221.com>
2020-06-10 21:48:38 +0530

    Yellow Balls Added and Score Card is now in Runnable State.

--- app/src/main/java/com/first75494/flyingfish/MainActivity.java
+++ app/src/main/java/com/first75494/flyingfish/MainActivity.java
@@ -3,15 +3,36 @@ package com.first75494.flyingfish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     private FlyingFish gameView;
+    private Handler handler = new Handler();
+    private final static long Interval=10;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         gameView = new FlyingFish(this);
         setContentView(gameView);
+
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        gameView.invalidate();
+                    }
+                });
+            }
+        },0,Interval);
     }
 }
