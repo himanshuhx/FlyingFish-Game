3c0faae436db9c18df5057667ac5cfc9b2b16533
himanshuhx <himanshuhx1221.com>
2020-06-10 19:41:52 +0530

    Score Card and background added.

--- app/src/main/java/com/first75494/flyingfish/MainActivity.java
+++ app/src/main/java/com/first75494/flyingfish/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FlyingFish gameView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        gameView = new FlyingFish(this);
+        setContentView(gameView);
     }
 }
--- app/src/main/java/com/first75494/flyingfish/SplashActivity.java
+++ app/src/main/java/com/first75494/flyingfish/SplashActivity.java
@@ -2,6 +2,7 @@ package com.first75494.flyingfish;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
@@ -10,5 +11,28 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread thread=new Thread(){
+            @Override
+            public void run() {
+                try{
+                   sleep(4000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent main=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(main);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
